900aaa29f1852eb1d85856bcb52d886fb8e8159d
Mahmoud Garwallane <garwallanemahmoud@gmail.com>
2023-09-30 12:55:55 +0100

    fix:drag interaction and numbers test

--- lib/main.dart
+++ lib/main.dart
@@ -32,25 +32,19 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   double xAddition = 0;
   double yAddition = 0;
-  double sensibility = 10;
+  double sensibility = 0.3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GestureDetector(
-        onVerticalDragUpdate: (DragUpdateDetails details) {
+        onPanUpdate: (details) {
           setState(() {
-            yAddition += details.primaryDelta ?? 0 * sensibility;
+            yAddition += details.delta.dy * sensibility;
+            xAddition += details.delta.dx * sensibility;
           });
         },
-        onHorizontalDragUpdate: (DragUpdateDetails details) {
-          setState(() {
-            xAddition += details.primaryDelta ?? 0 * sensibility;
-          });
-        },
-        onHorizontalDragStart: (DragStartDetails details) {},
-        onHorizontalDragEnd: (DragEndDetails details) {},
         child: Container(
-          width: double.infinity,
+          color: Colors.black,
           height: double.infinity,
           child: CustomPaint(
             painter: MyPainter(xAddition: xAddition, yAddition: yAddition),
@@ -59,7 +53,13 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.deepPurple, onPressed: () {}),
+          backgroundColor: Colors.deepPurple,
+          onPressed: () {
+            setState(() {
+              xAddition = 0;
+              yAddition = 0;
+            });
+          }),
     );
   }
 }
@@ -74,13 +74,33 @@ class MyPainter extends CustomPainter {
       ..color = Colors.deepPurple
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2.0;
-
+    double layoutSize = double.maxFinite;
     canvas.translate(size.width / 2 + xAddition, size.height / 2 + yAddition);
-    canvas.drawLine(Offset(0, 0), Offset(double.maxFinite, 0), paint);
-    canvas.drawLine(Offset(0, 0), Offset(-double.maxFinite, 0), paint);
-    canvas.drawLine(Offset(0, 0), Offset(0, double.maxFinite), paint);
-    canvas.drawLine(Offset(0, 0), Offset(0, -double.maxFinite), paint);
-    print(double.maxFinite);
+    canvas.drawLine(Offset(0, 0), Offset(layoutSize, 0), paint);
+    canvas.drawLine(Offset(0, 0), Offset(-layoutSize, 0), paint);
+    canvas.drawLine(Offset(0, 0), Offset(0, layoutSize), paint);
+    canvas.drawLine(Offset(0, 0), Offset(0, -layoutSize), paint);
+
+    void addNumbers() {
+      for (int i = 0; i < 200; i += 20) {
+        print(i);
+        final TextPainter textPainter = TextPainter(
+            text: TextSpan(
+              text: (i / 20).toInt().toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+            textAlign: TextAlign.justify,
+            textDirection: TextDirection.ltr)
+          ..layout(maxWidth: 5);
+        if (i == 0) {
+          textPainter.paint(canvas, Offset(i + -10, 2));
+        } else {
+          textPainter.paint(canvas, Offset(i - 5, 2));
+        }
+      }
+    }
+
+    addNumbers();
   }
 
   @override
