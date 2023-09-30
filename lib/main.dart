d626d64e77561110f22e1a346b3db1a5268c91c3
Mahmoud Garwallane <garwallanemahmoud@gmail.com>
2023-09-30 22:50:25 +0100

    feat: auto numbers trial by fahl

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 
@@ -70,22 +71,45 @@ class MyPainter extends CustomPainter {
   final double xAddition;
   final double yAddition;
   const MyPainter({required this.xAddition, required this.yAddition});
-  void drawAxes(Canvas canvas,Size size) {
+
+  void drawAxes(Canvas canvas, Size size) {
     final paint = Paint()
       ..color = Colors.black
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2.0;
-    canvas.drawLine(Offset(0, 0), Offset(size.width+xAddition.abs(), 0), paint);
-    canvas.drawLine(Offset(0, 0), Offset(-(size.width+xAddition.abs()), 0), paint);
-    canvas.drawLine(Offset(0, 0), Offset(0, size.height+yAddition.abs()), paint);
-    canvas.drawLine(Offset(0, 0), Offset(0, -(size.height+yAddition.abs())), paint);
+    canvas.drawLine(
+        Offset(0, 0), Offset(size.width + xAddition.abs(), 0), paint);
+    canvas.drawLine(
+        Offset(0, 0), Offset(-(size.width + xAddition.abs()), 0), paint);
+    canvas.drawLine(
+        Offset(0, 0), Offset(0, size.height + yAddition.abs()), paint);
+    canvas.drawLine(
+        Offset(0, 0), Offset(0, -(size.height + yAddition.abs())), paint);
+  }
+
+  void writeNumber(int i, Offset offset, Canvas canvas) {
+    final TextPainter textPainter = TextPainter(
+        text: TextSpan(
+          text: (i).toInt().toString(),
+          style: const TextStyle(color: Colors.black),
+        ),
+        textAlign: TextAlign.justify,
+        textDirection: TextDirection.ltr)
+      ..layout(minWidth: 10, maxWidth: 100);
+    textPainter.paint(canvas, offset);
+    canvas.drawLine(
+        Offset(offset.dx + 4, offset.dy + 2),
+        Offset(offset.dx + 4, -4),
+        Paint()
+          ..color = Colors.black
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 1);
   }
 
   @override
   void paint(Canvas canvas, Size size) {
     canvas.translate(size.width / 2 + xAddition, size.height / 2 + yAddition);
-    drawAxes(canvas,size);
-
+    drawAxes(canvas, size);
 
     void axesLength() {
       double xPositiveLength = size.width / 2 - xAddition;
@@ -99,10 +123,28 @@ class MyPainter extends CustomPainter {
       print("yNegativeLength: $yNegativeLength");
     }
 
-    axesLength();
-
+    double xPositiveLength = size.width / 2 - xAddition;
+    print("xPositiveLength: $xPositiveLength");
+    if (xPositiveLength / 2 > 1) {
+      for (var i = 0; i < size.width; i += 50) {
+        var ayism = (size.width / 50).floor();
+        writeNumber(
+            ayism - i, Offset((50 * (ayism - i)).toDouble(), 0), canvas);
+      }
+      var fahl = xPositiveLength -
+          ((xPositiveLength / size.width).floor() * size.width);
+      print("fahl: $fahl");
+      // axesLength();
+      // for (var i = 1; i < xPositiveLength / 50; i++) {
+      //   writeNumber(i, Offset((50 * i).toDouble(), 0), canvas);
+      // }
+      // writeNumber(2, Offset(100, 0), canvas);
+      // writeNumber(3, Offset(150, 0), canvas);
+    }
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
 }
